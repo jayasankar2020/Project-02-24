92947ac4b1cb83bd964b8177634a389a15829c35
jayasankar2020 <sjayasankar96@gmail.com>
2020-02-23 21:51:42 -0800

    projectdes

---
+++ src/test/java/org/cts/test/Test.java
@@ -0,0 +1,10 @@
+package org.cts.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\eclipse-jayasankar\\Selenium\\drivers\\chromedriver.exe");
+		}
+	
+	
+}
